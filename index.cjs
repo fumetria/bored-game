2a6ec101169e90edbfae81afb0465630009d3ce2
Fu Jun <fulin.fujun@outlook.es>
2025-09-22 12:53:56 +0200

    Change index.js extension for commonjs cjs and add routes for submit scores and get scores from database

---
+++ index.cjs
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const port = 4500;
+const dbQuerys = require("./database/db-querys.js");
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/submit-score", (req, res) => {
+    console.log("submitting a score");
+    try {
+        dbQuerys.createScore(req.body);
+        res.json({ status: 201, received: req.body });
+    } catch (error) {
+        res.json({ status: 422, received: req.body });
+    }
+})
+
+app.get("/scores", async (req, res) => {
+    console.log("getting socres...");
+    try {
+        const scores = await dbQuerys.getScores();
+        console.log(scores);
+        res.json({ status: 201, data: scores });
+    } catch (error) {
+        res.json({ status: 404, message: "Error getting data..." });
+    }
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+    console.log(`Game running on port: ${port}`);
+});
